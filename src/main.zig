c108f41bcb4bb9ed1f72c5e091321912a1825754
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2025-12-27 22:54:59 +0900

    feat: def Base64 struct

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,33 @@
 const std = @import("std");
 const zig_base64_encoder = @import("zig_base64_encoder");
 
+const Base64 = struct {
+    _table: *const [64]u8,
+
+    pub fn init() Base64 {
+        const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lower = "abcdefghijklmnopqrstuvwxyz";
+        const numbers_symb = "0123456789+/";
+        return Base64{
+            ._table = upper ++ lower ++ numbers_symb,
+        };
+    }
+
+    pub fn _char_at(self: Base64, index: usize) u8 {
+        return self._table[index];
+    }
+};
+
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
     try zig_base64_encoder.bufferedPrint();
+
+    const base64 = Base64.init();
+    std.debug.print(
+        "Chracter at index 28: {c}\n",
+        .{base64._char_at(28)}
+    );
 }
 
 test "simple test" {
